c3bbb9818941c4f16d557bd13324d139e9a386b1
Shaitanbabai <rebootleo0522@gmail.com>
2024-06-25 12:13:57 +0300

    Update PS03_BSoup2.py

--- PS03_BSoup2.py
+++ PS03_BSoup2.py
@@ -1,23 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
+from googletrans import Translator
 
 
 def get_english_words():
-    url = "https:/randomworld.com/"
+    url = "https://randomword.com/"
     try:
         response = requests.get(url)
-
         soup = BeautifulSoup(response.content, 'html.parser')
         english_words = soup.find("div", id="random_word").text.strip()
         word_definition = soup.find("div", id="random_word_definition").text.strip()
-
         return {
             "english_words": english_words,
             "word_definition": word_definition
         }
+    except IndexError as e:
+        print(f"Произошла ошибка при получении данных: {e}")
+        return None
 
-    except:
-        print("Произошла ошибка")
+def translate_definition(definition):
+    translator = Translator()
+    translated = translator.translate(definition, dest="ru")
+    return translated.text
 
 
 def word_game():
@@ -28,18 +32,17 @@ def word_game():
         word = word_dict.get("english_words")
         word_definition = word_dict.get("word_definition")
 
-        print(f"значение_слова, {word_definition}")
-        user = input("Что это за слово?")
+        print(f"Значение слова: {translate_definition(word_definition)}")
+        user = input("Что это за слово? ")
 
         if user == word:
-            print("Всё верно")
+            print("Всё верно!")
         else:
-            print(f"ответ не верный, было загадано слово - {word}")
+            print(f"Ответ неверный. Было загадано слово - {word}")
 
-        play_again = input("Хотите сыграть еще раз? y/n")
-        if play_again != "y":
-            print("Спасибо и до свиданья")
-            break
+        play_again = input("Хотите сыграть еще раз? (y/n) ")
+        if play_again.lower() != "y":
+            print("Спасибо и до свидания!")
 
 
 word_game()
